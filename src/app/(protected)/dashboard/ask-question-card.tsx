c7dc1214ecd4df4ac543c5e0c182a131d6be7d4a
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-09-03 13:33:21 +0530

    implement askQuestion RAG function for answering the questions based on repo

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -7,15 +7,33 @@ import useProject from "@/hooks/use-project";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { useState, type FormEvent } from "react";
 import Image from "next/image";
+import { askQuestion } from "./action";
+import { readStreamableValue } from "@ai-sdk/rsc";
 
 const AskQuestionCard = () => {
     const {project} = useProject();
     const [open, setOpen] = useState(false);
     const [question, setQuestion] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [filesReferences, setFilesReferences] = useState<{fileName: string; sourceCode: string; summary: string}[]>([]);
+    const [answer, setAnswer] = useState("");
 
     const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        if (!project?.id) return;
+
+        setLoading(true);
         setOpen(true);
+
+        const {output, filesReferences} = await askQuestion(question, project.id);
+        setFilesReferences(filesReferences);
+
+        for await (const delta of readStreamableValue(output)){
+            if(delta){
+                setAnswer(ans => ans +delta);
+            }
+        }
+        setLoading(false);
     }
 
   return (
@@ -27,6 +45,10 @@ const AskQuestionCard = () => {
                         <Image src="/logo.jpeg" alt="RepoMind" width={40} height={40}/>
                     </DialogTitle>
                 </DialogHeader>
+                {answer}
+                {filesReferences.map( (file, index) => {
+                    return <span key={index}>{file.fileName}</span>
+                })}
             </DialogContent>
         </Dialog>
         <Card className="relative col-span-3">
