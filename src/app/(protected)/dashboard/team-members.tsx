f99da5dfcc388ca375cd8ac79eaa7f3f8afe2420
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-09-22 01:47:09 +0530

    Display the current working member of  project, implement route function for that

---
+++ src/app/(protected)/dashboard/team-members.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import useProject from "@/hooks/use-project";
+import { api } from "@/trpc/react";
+
+
+const TeamMembers = () => {
+
+    const {projectId} = useProject();
+    const {data: members} = api.project.getTeamMembers.useQuery({projectId});
+
+  return (
+    <div className="flex items=center gap-2">
+        {
+            members?.map((member) => (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img key={member.id} src={member.user.imageUrl || ''} alt={member.user.firstName || ''} height={30} width={30} className="w-8 h-8 rounded-full" />
+            ))
+        }
+    </div>
+  )
+}
+
+export default TeamMembers;
